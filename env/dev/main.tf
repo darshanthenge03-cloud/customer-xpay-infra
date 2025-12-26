306fbc0ea96ac8daf7e14f94d80ccf7e6dac566a
unknown <darshanthenge03@gmail.com>
2025-12-26 09:53:01 +0000

    Create Azure Key Vault

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -3,9 +3,36 @@ provider "azurerm" {
   resource_provider_registrations = "none"
 }
 
+data "azurerm_client_config" "current" {}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group
   location = var.location
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                = "kv-xpay-dev-001"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  sku_name  = "standard"
+
+  soft_delete_retention_days = 7
+  purge_protection_enabled   = false
+}
+
+resource "azurerm_key_vault_access_policy" "terraform" {
+  key_vault_id = azurerm_key_vault.kv.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = data.azurerm_client_config.current.object_id
+
+  secret_permissions = [
+    "Get",
+    "List",
+    "Set",
+    "Delete"
+  ]
 }
 
 module "network" {
