c16cdef792605aedfc9c9b8170d45f44f3da53cd
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-01-28 15:15:18 +0530

    Fix source URL for static web app module

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -17,7 +17,7 @@ resource "azurerm_resource_group" "rg" {
 }
 
 module "staticwebapp" {
-  source = "git::https://github.com/darshanthenge03-cloud/terraform-azure-modules.git//static-web-app"
+  source = "git::https://github.com/darshanthenge03-cloud/terraform-azure-modules.git//staticwebapp"
 
   name                = "customer-static-webapp"
   resource_group_name = azurerm_resource_group.rg.name
