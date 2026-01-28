c05783b175fb74d2f19781a6d729e0204597680f
darshanthenge03-cloud <darshanthenge03@gmail.com>
2026-01-28 12:01:31 +0530

    Remove resource group from main.tf
    Removed resource group definition from main.tf.

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -2,14 +2,6 @@ provider "azurerm" {
   features {}
 }
 
-# --------------------
-# Resource Group
-# --------------------
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-}
-
 module "staticwebapp" {
   source = "git::https://github.com/darshanthenge03-cloud/terraform-azure-modules.git//staticwebapp?ref=main"
 
